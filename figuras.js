9d97d999c7d399af9d5bf9b03d42609e50f7122c
Fabricio Villalobos <fabricio33@gmail.com>
2021-08-03 21:18:51 -0500

    agregué los prieros cambios

---
+++ figuras.js
@@ -0,0 +1,40 @@
+console.log("Figuras Geométricas");
+
+//cuadrado
+console.group("Cuadrado");
+
+const ladoCuadrado = 5;
+console.log("Lados del cuadraro miden: " + ladoCuadrado + " cm");
+
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log("El perímetro del cuadrado es: " + perimetroCuadrado + " cm");
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+console.log("El área del cuadrado es: " + areaCuadrado + " cm^2");
+
+console.groupEnd();
+
+//Triángulo
+console.group("Triángulo");
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 6;
+const baseTriangulo = 4;
+const alturaTriángulo = 5.5;
+
+console.log(
+    "Los lados del triángulo miden: " 
+    + ladoTriangulo1 + "cm, " 
+    + ladoTriangulo2 + "cm, " 
+    + baseTriangulo + "cm "
+);
+console.log("La altura del triángulo es de: " + alturaTriángulo + " cm");
+
+const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + baseTriangulo;
+console.log("El perímetro del triángulo es: " + perimetroTriangulo + " cm");
+
+const areaTriangulo = (baseTriangulo * alturaTriángulo) / 2;
+console.log("El área del triángulo es: " + areaTriangulo + " cm^2");
+
+console.groupEnd();
+
+//círculo
